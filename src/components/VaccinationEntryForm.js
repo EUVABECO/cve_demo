cce061536d1ecddf1d95005ab491833b06bd8549
Mathieu Laporte <mlaporte@mesvaccins.net>
2024-10-04 17:24:15 +0200

    sort and filter vaccines

--- src/components/VaccinationEntryForm.js
+++ src/components/VaccinationEntryForm.js
@@ -5,11 +5,24 @@ export const VaccinationEntryForm = (props) => {
   if (nuva === null || nuva === undefined) {
     return <></>;
   }
+  const sortedVaccines = nuva.repositories.vaccines.all()
+  .filter(v => v.codes.length > 8) // limit vaccine list
+  .sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  })
   return (
     <Box p={4} borderWidth="1px" borderRadius="lg">
       <Select variant='outline' placeholder='Vaccine' onChange={(e) => setNuvaId(e.target.value)}>
         {
-          nuva.repositories.vaccines.all().map(vaccine => (
+          sortedVaccines.map(vaccine => (
             <option value={vaccine.code}>{vaccine.name}</option>
           ))
         }
